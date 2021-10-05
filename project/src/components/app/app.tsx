211cea53a1533dd5220f40126273f499ca9ca474
Inna <i.m.makhova@gmail.com>
2021-10-05 13:18:57 +0300

    Добавлена маршрутизация некоторых компонентов

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import FilmCard from '../film-card/film-card';
+import AddReview from '../add-review/add-review';
+import Player from '../player/player';
+import Page404 from '../page-404/page-404';
 
 type AppScreenProps = {
   title: string;
@@ -8,11 +16,35 @@ type AppScreenProps = {
 
 function App({title, genre, year} : AppScreenProps): JSX.Element {
   return (
-    <WelcomeScreen
-      title = {title}
-      genre = {genre}
-      year = {year}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Root}>
+          <WelcomeScreen
+            title = {title}
+            genre = {genre}
+            year = {year}
+          />
+        </Route>
+        <Route exact path={AppRoute.Login}>
+          <SignIn />
+        </Route>
+        <Route exact path={AppRoute.MyList}>
+          <MyList />
+        </Route>
+        <Route exact path={AppRoute.FilmCard}>
+          <FilmCard />
+        </Route>
+        <Route exact path={AppRoute.AddReview}>
+          <AddReview />
+        </Route>
+        <Route exact path={AppRoute.Player}>
+          <Player />
+        </Route>
+        <Route>
+          <Page404 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/const.ts
@@ -0,0 +1,8 @@
+export enum AppRoute {
+  Root = '/',
+  Login = '/login',
+  MyList = '/mylist',
+  FilmCard = '/films/:id',
+  AddReview = '/films/:id/review',
+  Player = '/player/:id',
+}
