25791edb43447a9009c111929d5ed5d60c807352
Inna <i.m.makhova@gmail.com>
2021-11-01 13:38:39 +0200

    Подключение функций thunk, applyMiddleware & createAPI

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/app';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 //import films from './mocks/films';
+import App from './components/app/app';
 import {reducer} from './store/reducer';
+import {createAPI} from './services/api';
 
 const store = createStore(
   reducer,
