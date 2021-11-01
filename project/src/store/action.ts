2183bb5aa242a941286882c2d8a66002d544e069
Inna <i.m.makhova@gmail.com>
2021-11-01 10:17:55 +0200

    Добавлены состояния счетчика для вывода фильмов, лимит по количеству фильмов, типы действия изменения счетчика и лимита фильмов, действия сброса счетчиков (для переключения между жанрами)

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,4 +1,11 @@
-import {ActionType, ChangeGenreAction, GetFilmsAction} from '../types/action';
+import {
+  ActionType,
+  ChangeFilmNumberLimit,
+  ChangeGenreAction,
+  ChangeLimitCounter,
+  GetFilmsAction,
+  ResetFilmNumberLimit
+} from '../types/action';
 import films from '../mocks/films';
 
 export const changeGenre = (genre: string): ChangeGenreAction => ({
@@ -9,4 +16,16 @@ export const changeGenre = (genre: string): ChangeGenreAction => ({
 export const getFilms = (): GetFilmsAction => ({
   type: ActionType.GetFilms,
   payload: films,
+});
+
+export const changeLimitCounter = (): ChangeLimitCounter => ({
+  type: ActionType.ChangeLimitCounter,
+});
+
+export const changeFilmNumberLimit = (): ChangeFilmNumberLimit => ({
+  type: ActionType.ChangeFilmNumberLimit,
+});
+
+export const resetFilmNumberLimit = (): ResetFilmNumberLimit => ({
+  type: ActionType.ResetFilmNumberLimit,
 });
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,12 +1,15 @@
-import {ActionType, Actions} from '../types/action';
+import {Actions, ActionType} from '../types/action';
 import {State} from '../types/state';
 import films from '../mocks/films';
+import {FILMS_NUMBER_STEP} from '../const';
 
 const initialFilms = films.slice();
 
 const initialState = {
   currentGenre: 'All genres',
   films: initialFilms,
+  limitCounter: 1,
+  filmNumberLimit: FILMS_NUMBER_STEP,
 } as State;
 
 const reducer = (state: State = initialState, action: Actions): State => {
@@ -15,6 +18,12 @@ const reducer = (state: State = initialState, action: Actions): State => {
       return {...state, currentGenre: action.payload};
     case ActionType.GetFilms:
       return {...state, films: action.payload};
+    case ActionType.ChangeLimitCounter:
+      return {...state, limitCounter: state.limitCounter + 1};
+    case ActionType.ChangeFilmNumberLimit:
+      return {...state, filmNumberLimit: state.filmNumberLimit * state.limitCounter};
+    case ActionType.ResetFilmNumberLimit:
+      return {...state, limitCounter: initialState.limitCounter, filmNumberLimit: initialState.filmNumberLimit};
     default:
       return state;
   }
