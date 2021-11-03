f36da69f98146928a9aa0a2ead97c5ecc70d0909
Inna <i.m.makhova@gmail.com>
2021-11-03 16:36:19 +0200

    В апи добавлен адаптер для данных от сервера, добавлен фетч для загрузки промо-фильма

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,14 +1,40 @@
 import {ThunkActionResult} from '../types/action';
-import {loadFilms, requireAuthorization, requireLogout} from './action';
+import {loadFilms, loadPromoFilm, requireAuthorization, requireLogout} from './action';
 import {saveToken, dropToken, Token} from '../services/token';
 import {APIRoute, AuthorizationStatus} from '../const';
 import {Film} from '../types/film';
 import {AuthData} from '../types/auth-data';
 
+function adaptToClient(film: any): Film {
+  return Object.assign(
+    {},
+    film,
+    {
+      posterImage: film.poster_image,
+      previewImage: film.preview_image,
+      backgroundImage: film.background_image,
+      backgroundColor: film.background_color,
+      videoLink: film.video_link,
+      previewVideoLink: film.preview_video_link,
+      scoresCount: film.scores_count,
+      runTime: film.runTime,
+      isFavorite: film.is_favorite,
+    },
+  );
+}
+
+export const fetchPromoFilmAction = (): ThunkActionResult =>
+  async (dispatch, _getState, api): Promise<void> => {
+    const {data} = await api.get<Film>(APIRoute.PromoFilm);
+    const adaptedData = adaptToClient(data);
+    dispatch(loadPromoFilm(adaptedData));
+  };
+
 export const fetchFilmAction = (): ThunkActionResult =>
   async (dispatch, _getState, api): Promise<void> => {
     const {data} = await api.get<Film[]>(APIRoute.Films);
-    dispatch(loadFilms(data));
+    const adaptedData = data.map((film) => adaptToClient(film));
+    dispatch(loadFilms(adaptedData));
   };
 
 export const checkAuthAction = (): ThunkActionResult =>
