df2a010e32a95f151c7afc945978c4bd8565e83f
Inna <i.m.makhova@gmail.com>
2021-11-03 16:37:49 +0200

    Подключение thunk к компонентам приложения

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -4,23 +4,33 @@ import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
-//import films from './mocks/films';
 import App from './components/app/app';
 import {reducer} from './store/reducer';
 import {createAPI} from './services/api';
+import {requireAuthorization} from './store/action';
+import {fetchFilmAction, checkAuthAction, fetchPromoFilmAction} from './store/api-actions';
+import {ThunkAppDispatch} from './types/action';
+import {AuthorizationStatus} from './const';
+
+const api = createAPI(
+  () => store.dispatch(requireAuthorization(AuthorizationStatus.NoAuth)),
+);
 
 const store = createStore(
   reducer,
-  composeWithDevTools(),
+  composeWithDevTools(
+    applyMiddleware(thunk.withExtraArgument(api)),
+  ),
 );
+
+(store.dispatch as ThunkAppDispatch)(checkAuthAction());
+(store.dispatch as ThunkAppDispatch)(fetchFilmAction());
+(store.dispatch as ThunkAppDispatch)(fetchPromoFilmAction());
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store = {store}>
-      <App
-        film={films[5]}
-        films={films}
-      />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
