0185ca1e13ad85e4f96bdf444fb67f53c473727d
Inna <i.m.makhova@gmail.com>
2021-11-06 18:02:35 +0200

    Отрефакторена маршрутизация

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import {connect, ConnectedProps} from 'react-redux';
-import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {Switch, Route, Router as BrowserRouter} from 'react-router-dom';
 import {AppRoute} from '../../const';
 import LoadingScreen from '../loading-screen/loading-screen';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
@@ -12,11 +12,11 @@ import Page404 from '../page-404/page-404';
 import PrivateRoute from '../private-route/private-route';
 import ReviewForm from '../review-form/review-form';
 import {State} from '../../types/state';
+import browserHistory from '../../browser-history';
 
-const mapStateToProps = ({promoFilm, films, authorizationStatus, isDataLoaded}: State) => ({
+const mapStateToProps = ({promoFilm, films, isDataLoaded}: State) => ({
   promoFilm,
   films,
-  //authorizationStatus,
   isDataLoaded,
 });
 
@@ -25,7 +25,7 @@ const connector = connect(mapStateToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 function App(props : PropsFromRedux): JSX.Element {
-  const {promoFilm, films, /*authorizationStatus, */isDataLoaded} = props;
+  const {promoFilm, films, isDataLoaded} = props;
 
   if (!isDataLoaded) {
     return (
@@ -34,7 +34,7 @@ function App(props : PropsFromRedux): JSX.Element {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter history={browserHistory}>
       <Switch>
         <Route exact path={AppRoute.Root}>
           <WelcomeScreen />
@@ -48,7 +48,7 @@ function App(props : PropsFromRedux): JSX.Element {
         <PrivateRoute
           exact
           path={AppRoute.MyList}
-          render={() => <MyList myFilms={films}/>}
+          render={({history}) => <MyList myFilms={films}/>}
         >
         </PrivateRoute>
         <PrivateRoute
