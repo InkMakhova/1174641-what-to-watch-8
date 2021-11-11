5c7d1ccaf2f4b1eec2603af07173b65e4a517273
Inna <i.m.makhova@gmail.com>
2021-11-11 18:49:13 +0200

    Рефакторинг store на типы состояний

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -3,16 +3,39 @@ import {Film} from './film';
 import {User} from './user';
 import {FilmReview} from './film-review';
 
-export type State = {
-  currentGenre: string;
-  promoFilm: Film;
+// export type State = {
+//   currentGenre: string;
+//   promoFilm: Film;
+//   films: Film[];
+//   currentFilm: Film;
+//   similarFilms: Film[];
+//   comments: FilmReview[];
+//   limitCounter: number;
+//   filmNumberLimit: number;
+//   authorizationStatus: AuthorizationStatus,
+//   isDataLoaded: boolean,
+//   user: User,
+// }
+
+export type FilmsData = {
   films: Film[];
+  promoFilm: Film;
+  isDataLoaded: boolean,
+}
+
+export type FilmListProcess = {
+  currentGenre: string;
+  limitCounter: number;
+  filmNumberLimit: number;
+}
+
+export type FilmInfoProcess = {
   currentFilm: Film;
   similarFilms: Film[];
   comments: FilmReview[];
-  limitCounter: number;
-  filmNumberLimit: number;
-  authorizationStatus: AuthorizationStatus,
-  isDataLoaded: boolean,
-  user: User,
+}
+
+export type UserProcess = {
+  authorizationStatus: AuthorizationStatus;
+  user: User;
 }
