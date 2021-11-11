68570229989803ae74c41780bba795a5cc48a745
Inna <i.m.makhova@gmail.com>
2021-11-11 20:43:14 +0200

    Добавлен redux-toolkit

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+//import {createStore, applyMiddleware} from 'redux';
+import {configureStore} from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './components/app/app';
 import {createAPI} from './services/api';
@@ -17,13 +18,23 @@ const api = createAPI(
   () => store.dispatch(requireAuthorization(AuthorizationStatus.NoAuth)),
 );
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk.withExtraArgument(api)),
-    applyMiddleware(redirect),
-  ),
-);
+// export const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(
+//     applyMiddleware(thunk.withExtraArgument(api)),
+//     applyMiddleware(redirect),
+//   ),
+// );
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }).concat(redirect),
+});
 
 (store.dispatch as ThunkAppDispatch)(checkAuthAction());
 (store.dispatch as ThunkAppDispatch)(fetchFilmAction());
